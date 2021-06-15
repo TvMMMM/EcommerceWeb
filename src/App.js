3adffd2e940cf75f3dd814ce3793b3ed84a53a1f
Minh <viminh.truong18@gmail.com>
2021-06-15 20:46:44 +0700

    share code to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import data from './Data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+return (
+    <div className="grid-container">
+        <header className="row">
+            <div>
+                <a className="brand" href="index.html">TvM Shop</a>
+            </div>
+            <div>
+                <a href="cart.html">Cart</a>
+                <a href="signin.html">Sign In</a>
+            </div>
+        </header>
+        <main>
+            <div className="row center">
+                {data.products.map((product) => (
+                        <div key={product._id}  className="card">
+                            <a href={`/product/${product._id}`}>
+                                <img 
+                                src={product.image} 
+                                alt={product.name}
+                                />
+                            </a>
+                            <div className="card-body">
+                                <a href={`/product/${product._id}`}>
+                                    <h2>{product.name}</h2>
+                                </a>
+                                <div className="price">
+                                    ${product.price}
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }             
+            </div>
+        </main>
+        <footer className="row center">
+            Allright reserved
+        </footer>
     </div>
-  );
+);
 }
 
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,33 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'White T-Shirt',
+            category:'T-Shirt',
+            image:'/images/Wshirt.jpg',
+            price: 40,
+            brand:'No brand',
+            description:'Basic shirt with high quality materials'
+        },
+        {
+            _id:'2',
+            name:'White Adidas Shirt',
+            category:'T-Shirt',
+            image:'/images/Ashirt.jpg',
+            price: 60,
+            brand:'Adidas',
+            description:'Basic shirt with high quality materials'
+        },
+        {
+            _id:'3',
+            name:'White Nike Shirt',
+            category:'T-Shirt',
+            image:'/images/Nshirt.jpg',
+            price: 60,
+            brand:'Nike',
+            description:'Basic shirt with high quality materials'
+        },
+    ]
+}
+
+export default data;
