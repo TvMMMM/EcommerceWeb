a3817c543376e6da4281b83e057d6a56ec8775ca
Minh <viminh.truong18@gmail.com>
2021-06-15 20:57:54 +0700

    Create-components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Product from './components/Product';
 import data from './Data';
 
 function App() {
@@ -16,22 +17,7 @@ return (
         <main>
             <div className="row center">
                 {data.products.map((product) => (
-                        <div key={product._id}  className="card">
-                            <a href={`/product/${product._id}`}>
-                                <img 
-                                src={product.image} 
-                                alt={product.name}
-                                />
-                            </a>
-                            <div className="card-body">
-                                <a href={`/product/${product._id}`}>
-                                    <h2>{product.name}</h2>
-                                </a>
-                                <div className="price">
-                                    ${product.price}
-                                </div>
-                            </div>
-                        </div>
+                    <Product key={product._id} product={product}></Product>
                     ))
                 }             
             </div>
